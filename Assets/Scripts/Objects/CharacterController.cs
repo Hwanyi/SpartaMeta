71ce346500775c9ce8b180a70db4a95723953d04
Hwanyi <wksgo123@knu.ac.kr>
2025-02-19 18:06:25 +0900

    Add external assets and PlayerController

---
+++ Assets/Scripts/Objects/CharacterController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Vector2 movementDirection;
+    private Rigidbody2D _rigidbody;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _camera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = movementDirection * 5.0f;
+        _camera.transform.position = gameObject.transform.position + Vector3.back;
+    }
+}
